70e777bac64540f05336593b52a1d333466f5130
dqube <mdevendran@outlook.com>
2025-07-30 09:30:46 -0300

    feat: Reintroduce DomainEventWrapper class for event handling in CQRS architecture

--- BuildingBlocks.Application/CQRS/Events/DomainEventWrapper.cs
+++ BuildingBlocks.Application/CQRS/Events/DomainEventWrapper.cs
@@ -1,21 +1,21 @@
-using BuildingBlocks.Application.CQRS.Events;
 using BuildingBlocks.Domain.DomainEvents;
 
-namespace AuthService.Application.EventHandlers;
+namespace BuildingBlocks.Application.CQRS.Events;
 
 public class DomainEventWrapper<TDomainEvent> : IEvent
     where TDomainEvent : IDomainEvent
 {
     public TDomainEvent DomainEvent { get; }
-    public Guid Id { get; }
-    public DateTime OccurredOn { get; }
-    public string EventType { get; }
-
+    
     public DomainEventWrapper(TDomainEvent domainEvent)
     {
         DomainEvent = domainEvent;
         Id = domainEvent.Id;
         OccurredOn = domainEvent.OccurredOn;
-        EventType = typeof(TDomainEvent).Name;
+        EventType = domainEvent.GetType().Name;
     }
+
+    public Guid Id { get; }
+    public DateTime OccurredOn { get; }
+    public string EventType { get; }
 }
